2af54481f20c371559145892b2eb29f540f47577
Oaksas <aman.getnet2@gmail.com>
2021-01-19 19:31:25 -1100

    About first commit

---
+++ ASSETS/js/script.js
@@ -0,0 +1,20 @@
+
+ jQuery(document).ready(function(){
+    var navOffset = jQuery("#centerNav").offset().top;
+
+jQuery(window).scroll(function(){
+   var scrolPosition = jQuery(window).scrollTop();
+
+   if(scrolPosition >= navOffset){
+      jQuery("#centerNav").addClass("fixed");
+
+   }
+   else{
+      jQuery("#centerNav").removeClass("fixed");
+
+
+   }
+
+});
+    
+     });

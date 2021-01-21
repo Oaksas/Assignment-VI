283d0d7d2adbcfeacae84c9950e0c2e4e07d16e0
Oaksas <aman.getnet2@gmail.com>
2021-01-20 20:57:55 -1100

    Final Commit

--- ASSETS/js/script.js
+++ ASSETS/js/script.js
@@ -1,6 +1,5 @@
  jQuery(document).ready(function(){
     var navOffset = jQuery("#centerNav").offset().top;
-    alert(navOffset);
 
 jQuery(window).scroll(function(){
    var scrolPosition = jQuery(window).scrollTop();
